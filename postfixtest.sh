38206d2cfcc4d1c0c117dfab06df25b2debe9520
Kilian Ramharter <kilian.rmh@icloud.com>
2025-06-20 17:24:49 +0200

    Added demo script for postfix headless deployment

---
+++ postfixtest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MAILNAME="mail.example.com"
+MAILTYPE="Internet Site"  # Options: No configuration, Internet Site, Internet with smarthost, Satellite system, Local only
+
+echo "postfix postfix/mailname string $MAILNAME" | debconf-set-selections
+echo "postfix postfix/main_mailer_type select $MAILTYPE" | debconf-set-selections
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt install -y postfix
