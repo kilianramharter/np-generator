c2e293b2c9329ddd9212c8aaf0fd30b1c1bd02f4
Kilian Ramharter <kilian.rmh@icloud.com>
2025-06-25 13:33:16 +0200

    Finished netplan script

--- helpers/netplan-config.sh
+++ helpers/netplan-config.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# ./netplan-config.sh 180.1.10.0/24 180.1.10.1 180.1.10.5 fd00::0/64 fd00::1 fd00::5
-# ./netplan-config.sh {IPV4-CIDR} {IPV4-ADDRESS} {IPV4-NAMESERVER} [IPV6-CIDR] [IPV6-ADDRESS] [IPV6-NAMESERVER]
+# ./netplan-config.sh ens33 180.1.10.1/24 180.1.10.254 180.1.10.5 fd00::0/64 fd00::1 fd00::5
 
 # HINT! Run Script only as "sudo su"!
 if [ "$EUID" -ne 0 ]
@@ -10,40 +9,34 @@ if [ "$EUID" -ne 0 ]
 fi
 
 # Check if at least 3 arguments are passed
-if [ $# -lt 3 ]; then
+if [ $# -lt 4 ]; then
     echo -e "\033[1;31mMissing arguments.\033[0m"
-    echo -e "\033[1;31mUsage: $0 {IPV4-CIDR-ADDRESS} {IPV4-GATEWAY} {IPV4-NAMESERVER} [IPV6-CIDR-ADDRESS] [IPV6-GATEWAY] [IPV6-NAMESERVER]\033[0m"
+    echo -e "\033[1;31mUsage: $0 {INTERFACE-NAME} {IPV4-CIDR-ADDRESS} {IPV4-GATEWAY} {IPV4-NAMESERVER} [IPV6-CIDR-ADDRESS] [IPV6-GATEWAY] [IPV6-NAMESERVER]\033[0m"
+    exit 1
+fi
+
+SERVER_INTERFACE=$1
+
+IPV4_CIDR_ADDRESS="$2" 
+IPV4_GATEWAY="$3"
+IPV4_NAMESERVER="$4"
+
+IPV6_CIDR_ADDRESS="$5"
+IPV6_GATEWAY="$6"
+IPV6_NAMESERVER="$7"
+
+NETPLAN_DIR="/etc/netplan/"
+NETPLAN_FILENAME="100-$SERVER_INTERFACE-config.yaml"
+NETPLAN_FILE="$NETPLAN_DIR$NETPLAN_FILENAME"
+
+# Check if interface exists
+if ! ip link show "$SERVER_INTERFACE" > /dev/null 2>&1; then
+    echo -e "\033[1;31mError:\033[0m Interface '$SERVER_INTERFACE' does not exist."
     exit 1
 fi
 
 
-
-IPV4_CIDR_ADDRESS="$1" 
-IPV4_GATEWAY="$2"
-IPV4_NAMESERVER="$3"
-
-IPV6_CIDR_ADDRESS="$4"
-IPV6_GATEWAY="$5"
-IPV6_NAMESERVER="$6"
-
-SERVER_INTERFACE=ens33
-NETPLAN_DIR="/etc/netplan/"
-NETPLAN_FILE="${NETPLAN_DIR}50-cloud-init.yaml"
-
-#IPV4_SERVER_IP=180.1.10.1
-#IPV4_SERVER_SUBNETMASK=24
-#IPV4_SERVER_GATEWAY=180.1.10.254
-#IPV4_SERVER_NAMESERVER=180.1.10.5
-
-#IPV6_SERVER_IP=fd00::1
-#IPV6_SERVER_SUBNETMASK=64
-#IPV6_SERVER_NAMESERVER=fd00::5
-#IPV6_SERVER_GATEWAY=fd00::254
-
-
-# SERVER_HOSTNAME=ROOT-MX
-
-echo -n "Setting up networking... "
+echo -n "Generating netplan config for $SERVER_INTERFACE... "
 
 # cat > /etc/netplan/50-cloud-init.yaml <<EOF
 # network:
@@ -65,29 +58,46 @@ echo -n "Setting up networking... "
 #             - $IPV6_NAMESERVER
 # EOF
 
-echo -e "network:" > $NETPLAN_FILE
-echo -e "\tversion: 2"; >> $NETPLAN_FILE
-echo -e "\trenderer: networkd"; >> $NETPLAN_FILE
-echo -e "\tethernets:" >> $NETPLAN_FILE
-echo -e "\t\t$SERVER_INTERFACE:" >> $NETPLAN_FILE
-echo -e "\t\t\taddresses:" >> $NETPLAN_FILE
-echo -e "\t\t\t- $IPV4_CIDR_ADDRESS" >> $NETPLAN_FILE
+# Build the YAML file
+cat > "$NETPLAN_FILE" <<EOF
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    $SERVER_INTERFACE:
+      addresses:
+        - $IPV4_CIDR_ADDRESS
+EOF
+
 if [ -n "$IPV6_CIDR_ADDRESS" ]; then
-    echo -e "\t\t\t- $IPV6_CIDR_ADDRESS" >> $NETPLAN_FILE
-fi
-echo -e "\t\t\troutes:" >> $NETPLAN_FILE
-echo -e "\t\t\t - to: default" >> $NETPLAN_FILE
-echo -e "\t\t\t   via: $IPV4_GATEWAY" >> $
-if [ -n "$IPV6_GATEWAY" ]; then
-    echo -e "\t\t\t - to: default" >> $NETPLAN_FILE
-    echo -e "\t\t\t   via: $IPV6_GATEWAY" >> $NETPLAN_FILE
-fi
-echo -e "\t\t\tnameservers:" >> $NETPLAN_FILE
-echo -e "\t\t\t\taddresses:" >> $NETPLAN_FILE
-echo -e "\t\t\t\t- $IPV4_NAMESERVER" >> $NETPLAN_FILE
-if [ -n "$IPV6_NAMESERVER" ]; then
-    echo -e "\t\t\t\t- $IPV6_NAMESERVER" >> $NETPLAN_FILE
+    echo "        - $IPV6_CIDR_ADDRESS" >> "$NETPLAN_FILE"
 fi
 
+cat >> "$NETPLAN_FILE" <<EOF
+      routes:
+        - to: default
+          via: $IPV4_GATEWAY
+EOF
+
+if [ -n "$IPV6_GATEWAY" ]; then
+cat >> "$NETPLAN_FILE" <<EOF
+        - to: default
+          via: $IPV6_GATEWAY
+EOF
+fi
+
+cat >> "$NETPLAN_FILE" <<EOF
+      nameservers:
+        addresses:
+          - $IPV4_NAMESERVER
+EOF
+
+if [ -n "$IPV6_NAMESERVER" ]; then
+    echo "          - $IPV6_NAMESERVER" >> "$NETPLAN_FILE"
+fi
+
+echo -e "\e[1;32mdone\e[0m"
+
+echo -n "Applying new configuration... "
 netplan apply
 echo -e "\e[1;32mdone\e[0m"
